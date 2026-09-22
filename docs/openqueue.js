253af6f0c6816995b44108f5e3e8bf01de8f0907
grottijohm-cyber <grottijohm@gmail.com>
2026-09-21 20:54:48 -0700

    Auto-refresh permanent library from video drawer

--- docs/openqueue.js
+++ docs/openqueue.js
@@ -1,3 +1,6 @@
 'use strict';
 const openQueueButton=document.getElementById('openQueue');
 if(openQueueButton)openQueueButton.addEventListener('click',()=>document.getElementById('menuBtn')?.click());
+const videosTab=document.getElementById('tabVideos');
+if(videosTab)videosTab.addEventListener('click',()=>{if(typeof refreshLibrary==='function')void refreshLibrary(true)});
+window.addEventListener('online',()=>{if(typeof jobs!=='undefined'&&typeof jobActive==='function'&&typeof pollJob==='function'){for(const job of jobs.filter(jobActive))if(job.jobId)void pollJob(job.localId)}});
