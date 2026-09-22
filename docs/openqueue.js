41f2f1033dc24658d89975d65fab502d04c20f54
grottijohm-cyber <grottijohm@gmail.com>
2026-09-21 20:47:24 -0700

    Wire secondary queue button without inline script

---
+++ docs/openqueue.js
@@ -0,0 +1,3 @@
+'use strict';
+const openQueueButton=document.getElementById('openQueue');
+if(openQueueButton)openQueueButton.addEventListener('click',()=>document.getElementById('menuBtn')?.click());
